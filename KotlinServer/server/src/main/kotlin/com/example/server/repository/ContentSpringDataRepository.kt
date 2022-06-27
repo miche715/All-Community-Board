e5e1e8e5bce94188cb5234f93647570f4f433f01
Cheel <44915367+miche715@users.noreply.github.com>
2022-06-27 18:05:27 +0900

    리팩토링

--- KotlinServer/server/src/main/kotlin/com/example/server/repository/ContentSpringDataRepository.kt
+++ KotlinServer/server/src/main/kotlin/com/example/server/repository/ContentSpringDataRepository.kt
@@ -2,7 +2,6 @@ package com.example.server.repository
 
 import com.example.server.domain.Content
 import org.springframework.data.domain.Pageable
-import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ContentSpringDataRepository : JpaRepository<Content, Long>, ContentRepository
